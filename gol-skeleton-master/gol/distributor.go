dd08864a9f8c94d4ff206e17124137f71b14b7de
Will <iq20064@bristol.ac.uk>
2021-11-02 16:57:31 +0000

    Passes step 2 tests

--- gol-skeleton-master/gol/distributor.go
+++ gol-skeleton-master/gol/distributor.go
@@ -23,10 +23,11 @@ func distributor(p Params, c distributorChannels) {
 		currentWorld[i] = make([]byte, p.ImageHeight)
 	}
 	//Create a second 2D slice to store next state of the world (odd numbered turns)
-	nextWorld := make([][]byte, p.ImageWidth)
+	/*nextWorld := make([][]byte, p.ImageWidth)
 	for i := 0; i < p.ImageWidth; i++ {
 		nextWorld[i] = make([]byte, p.ImageHeight)
-	}
+	}*/
+	nextWorld := [][]byte{}
 
 	//TODO read in initial state of GOL using io.go
 	width := strconv.Itoa(p.ImageWidth)
@@ -41,26 +42,31 @@ func distributor(p Params, c distributorChannels) {
 			currentWorld[j][k] = <- c.ioInput
 		}
 	}
-	//fine up to here (works on 0 turns)
 
+	workerChannels := []chan [][]byte{}
+	for i := 0;i < p.Threads;i++ {
+		workerChannels = append(workerChannels, make(chan [][]byte))
+	}
 
 	// TODO: Execute all turns of the Game of Life.
 	turns := p.Turns
+	columnsPerChannel := len(currentWorld) / p.Threads
 	for turn := 0; turn < turns; turn++ {
-		for i := range currentWorld	{
-			for j := range currentWorld[i]	{
-				if getNumSurroundingCells(i, j, currentWorld) == 3 {
-					nextWorld[i][j] = 0xFF
-				}	else if getNumSurroundingCells(i, j, currentWorld) < 2	{
-					nextWorld[i][j] = 0
-				}	else if getNumSurroundingCells(i, j, currentWorld) > 3	{
-					nextWorld[i][j] = 0
-				}	else if getNumSurroundingCells(i, j, currentWorld) == 2 {
-					nextWorld[i][j] = currentWorld[i][j]
-				}
+		//Splitting up world and distributing to channels
+		for sliceNum := 0; sliceNum < p.Threads; sliceNum++{
+			go processNewSlice(workerChannels[sliceNum])
+			currentSlice := sliceWorld(sliceNum,columnsPerChannel,currentWorld)
+			workerChannels[sliceNum] <- currentSlice
+		}
+		//Reconstructing image from worker channels
+		for i := range workerChannels{
+			nextSlice := <- workerChannels[i]
+			for j := range nextSlice{
+				nextWorld = append(nextWorld, nextSlice[j])
 			}
 		}
 	}
+
 	//calculate the alive cells
 	aliveCells := make([]util.Cell, 0, p.ImageWidth * p.ImageHeight)
 	for i , _ := range currentWorld {
@@ -87,6 +93,45 @@ func distributor(p Params, c distributorChannels) {
 	close(c.events)
 }
 
+//Helper function for splitting the world into slices
+func sliceWorld(sliceNum int,columnsPerChannel int,currentWorld [][]byte) [][]byte{
+	currentSlice := [][]byte{}
+	//Adding extra column to back and front of slice to avoid lines of cells that aren't processed
+	extraBackColumnIndex := boundNumber(sliceNum * columnsPerChannel - 1,len(currentWorld))
+	extraFrontColumnIndex := boundNumber(sliceNum * columnsPerChannel + columnsPerChannel,len(currentWorld))
+	currentSlice = append(currentSlice,currentWorld[extraBackColumnIndex])
+	for i := 0;i < columnsPerChannel;i++{
+		currentSlice = append(currentSlice,currentWorld[sliceNum * columnsPerChannel + i])
+	}
+	currentSlice = append(currentSlice,currentWorld[extraFrontColumnIndex])
+	return currentSlice
+}
+
+//Sends the next state of a slice to the given channel, should be run as goroutine
+func processNewSlice(channel chan [][]byte) {
+	currentSlice := <- channel
+	//Making new slice to write changes to
+	nextSlice := make([][]byte, len(currentSlice))
+	for i := 0; i < len(currentSlice); i++ {
+		nextSlice[i] = make([]byte, len(currentSlice[i]))
+	}
+
+	for i := 1; i < len(currentSlice) - 2;i++	{
+		for j := range currentSlice[i]	{
+			if getNumSurroundingCells(i, j, currentSlice) == 3 {
+				nextSlice[i][j] = 0xFF
+			}	else if getNumSurroundingCells(i, j, currentSlice) < 2	{
+				nextSlice[i][j] = 0
+			}	else if getNumSurroundingCells(i, j, currentSlice) > 3	{
+				nextSlice[i][j] = 0
+			}	else if getNumSurroundingCells(i, j, currentSlice) == 2 {
+				nextSlice[i][j] = currentSlice[i][j]
+			}
+		}
+	}
+	channel <- nextSlice
+}
+
 //count number of active cells surrounding a current cell
 func getNumSurroundingCells(x int, y int, world [][]byte)	int{
 	const ALIVE = 0xFF
@@ -96,9 +141,9 @@ func getNumSurroundingCells(x int, y int, world [][]byte)	int{
 	var prevX = x - 1
 	var prevY = y - 1
 	succX = boundNumber(succX,len(world))
-	succY = boundNumber(succY,len(world))
+	succY = boundNumber(succY,len(world[0]))
 	prevX = boundNumber(prevX,len(world))
-	prevY = boundNumber(prevY,len(world))
+	prevY = boundNumber(prevY,len(world[0]))
 	if world[prevX][y] == ALIVE	{
 		counter++
 	}
