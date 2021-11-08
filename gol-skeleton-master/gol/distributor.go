63e31c67768109dd69cd241dac8d4eb6db7092ae
James-Millan <nf20792@bristol.ac.uk>
2021-11-08 14:32:44 +0000

    ticker test version

--- gol-skeleton-master/gol/distributor.go
+++ gol-skeleton-master/gol/distributor.go
@@ -3,6 +3,9 @@ package gol
 import (
 	"fmt"
 	"strconv"
+	"time"
+
+	//"time"
 	"uk.ac.bris.cs/gameoflife/util"
 )
 
@@ -22,17 +25,10 @@ func distributor(p Params, c distributorChannels) {
 	for i := 0; i < p.ImageWidth; i++ {
 		currentWorld[i] = make([]byte, p.ImageHeight)
 	}
-	//Create a second 2D slice to store next state of the world (odd numbered turns)
-	nextWorld := make([][]byte, p.ImageWidth)
-	for i := 0; i < p.ImageWidth; i++ {
-		nextWorld[i] = make([]byte, p.ImageHeight)
-	}
 
-	//TODO read in initial state of GOL using io.go
+	//read in initial state of GOL using io.go
 	width := strconv.Itoa(p.ImageWidth)
-	turnStr := strconv.Itoa(p.Turns)
-	filename := width + "x" + width + "x" + turnStr
-	fmt.Println(filename)
+	filename := width + "x" + width
 	c.ioCommand <- ioInput
 	c.ioFilename <- filename
 	//read file into current world
@@ -41,27 +37,93 @@ func distributor(p Params, c distributorChannels) {
 			currentWorld[j][k] = <- c.ioInput
 		}
 	}
-	//fine up to here (works on 0 turns)
+
+	workerChannels := []chan [][]byte{}
+	for i := 0;i < p.Threads;i++ {
+		workerChannels = append(workerChannels, make(chan [][]byte))
+	}
 
 	//Execute all turns of the Game of Life.
+
 	turns := p.Turns
-	for turn := 0; turn < turns; turn++ {
-		for i := range currentWorld	{
-			for j := range currentWorld[i]	{
-				if getNumSurroundingCells(i, j, currentWorld) == 3 {
-					nextWorld[i][j] = 0xFF
-				}	else if getNumSurroundingCells(i, j, currentWorld) < 2	{
-					nextWorld[i][j] = 0
-				}	else if getNumSurroundingCells(i, j, currentWorld) > 3	{
-					nextWorld[i][j] = 0
-				}	else if getNumSurroundingCells(i, j, currentWorld) == 2 {
-					nextWorld[i][j] = currentWorld[i][j]
-				}
+	//TODO make a ticker that sends the alive cells count down the events channel
+	ticker := time.NewTicker(2 * time.Second)
+	defer ticker.Stop()
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				cells := getAliveCellsCount(currentWorld)
+				c.events <- AliveCellsCount{CellsCount: cells}
+				fmt.Println("number of alive cells is " + strconv.Itoa(cells))
 			}
 		}
+	}()
+	columnsPerChannel := len(currentWorld) / p.Threads
+	for turn := 0; turn < turns; turn++ {
+		nextWorld := [][]byte{}
+		//Splitting up world and distributing to channels
+		remainderThreads := len(currentWorld) % p.Threads
+		offset := 0
+		for sliceNum := 0; sliceNum < p.Threads; sliceNum++{
+			go processNewSlice(workerChannels[sliceNum])
+			currentSlice := sliceWorld(sliceNum,columnsPerChannel,currentWorld,&remainderThreads,&offset)
+			workerChannels[sliceNum] <- currentSlice
+		}
+		//Reconstructing image from worker channels
+		for i := range workerChannels{
+			nextSlice := <- workerChannels[i]
+			for j := range nextSlice{
+				nextWorld = append(nextWorld, nextSlice[j])
+			}
+		}
+		//update current world.
+		for i := range nextWorld	{
+			for j := range nextWorld[i]	{
+				currentWorld[i][j] = nextWorld[i][j]
+			}
+		}
+		c.events <- TurnComplete{CompletedTurns: turn}
 	}
-	//calculate the alive cells
-	aliveCells := make([]util.Cell, 0, p.ImageWidth * p.ImageHeight)
+
+	//Report the final state using FinalTurnCompleteEvent.
+	c.events <- FinalTurnComplete{
+		CompletedTurns: turns,
+		Alive: getAliveCells(currentWorld)}
+	// Make sure that the Io has finished any output before exiting.
+	c.ioCommand <- ioCheckIdle
+	<-c.ioIdle
+
+	c.events <- StateChange{turns, Quitting}
+
+	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
+	close(c.events)
+}
+
+//Helper function for splitting the world into slices
+func sliceWorld(sliceNum int,columnsPerChannel int,currentWorld [][]byte,remainderThreads *int,offset *int) [][]byte{
+	currentSlice := [][]byte{}
+	//Adding extra column to back of slice to avoid lines of cells that aren't processed
+	extraBackColumnIndex := boundNumber(sliceNum * columnsPerChannel - 1 + *offset,len(currentWorld))
+	currentSlice = append(currentSlice,currentWorld[extraBackColumnIndex])
+	for i := 0;i < columnsPerChannel;i++{
+		currentSlice = append(currentSlice,currentWorld[sliceNum * columnsPerChannel + i + *offset])
+	}
+	//Adding extra column to this thread if the world doesn't split into each thread without remainders
+	if *remainderThreads > 0 {
+		*remainderThreads -= 1
+		*offset += 1
+		currentSlice = append(currentSlice,currentWorld[boundNumber(sliceNum * columnsPerChannel + columnsPerChannel - 1 + *offset,len(currentWorld))])
+	}
+	//Adding extra column to front of slice to avoid lines of cells that aren't processed
+	extraFrontColumnIndex := boundNumber(sliceNum * columnsPerChannel + columnsPerChannel + *offset,len(currentWorld))
+	currentSlice = append(currentSlice,currentWorld[extraFrontColumnIndex])
+	return currentSlice
+}
+
+//calculates the alive cells given the current world
+func getAliveCells(currentWorld [][]byte) []util.Cell	{
+	aliveCells := make([]util.Cell, 0)
 	for i , _ := range currentWorld {
 		for j, _ := range currentWorld[i] {
 			if currentWorld[i][j] == 0xFF	{
@@ -70,64 +132,91 @@ func distributor(p Params, c distributorChannels) {
 			}
 		}
 	}
+	return aliveCells
+}
 
-	//Report the final state using FinalTurnCompleteEvent.
-	c.events <- FinalTurnComplete{
-		CompletedTurns: turns,
-		Alive: aliveCells}
-	// Make sure that the Io has finished any output before exiting.
-	c.ioCommand <- ioCheckIdle
-	<-c.ioIdle
+//calculates the number of alive cells given the current world
+func getAliveCellsCount(currentWorld [][]byte) int	{
+	counter := 0
+	for i , _ := range currentWorld {
+		for j, _ := range currentWorld[i] {
+			if currentWorld[i][j] == 0xFF	{
+				counter++
+			}
+		}
+	}
+	return counter
+}
 
-	c.events <- StateChange{turns, Quitting}
-	
-	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
-	close(c.events)
+//Sends the next state of a slice to the given channel, should be run as goroutine
+func processNewSlice(channel chan [][]byte) {
+	currentSlice := <- channel
+	//Making new slice to write changes to
+	nextSlice := make([][]byte, len(currentSlice) - 2)
+	for i := range nextSlice{
+		nextSlice[i] = make([]byte, len(currentSlice[0]))
+	}
+	for i := 1;i < len(currentSlice) - 1;i++	{
+		for j := range currentSlice[i]	{
+			if getNumSurroundingCells(i, j, currentSlice) == 3 {
+				nextSlice[i-1][j] = 0xFF
+			}	else if getNumSurroundingCells(i, j, currentSlice) < 2	{
+				nextSlice[i-1][j] = 0
+			}	else if getNumSurroundingCells(i, j, currentSlice) > 3	{
+				nextSlice[i-1][j] = 0
+			}	else if getNumSurroundingCells(i, j, currentSlice) == 2 {
+				nextSlice[i-1][j] = currentSlice[i][j]
+			}
+		}
+	}
+	channel <- nextSlice
 }
 
 //count number of active cells surrounding a current cell
 func getNumSurroundingCells(x int, y int, world [][]byte)	int{
+	const ALIVE = 0xFF
 	var counter = 0
 	var succX = x + 1
 	var succY = y + 1
 	var prevX = x - 1
 	var prevY = y - 1
-	if	succX >= len(world)	{
-		succX = 0
-	}
-	if succY >= len(world[x])	{
-		succY = 0
-	}
-	if prevX < 0	{
-		prevX = len(world) - 1
-	}
-	if prevY < 0	{
-		prevY = len(world[x]) - 1
-	}
-	if world[prevX][y] == 0xFF	{
+	succX = boundNumber(succX,len(world))
+	succY = boundNumber(succY,len(world[0]))
+	prevX = boundNumber(prevX,len(world))
+	prevY = boundNumber(prevY,len(world[0]))
+	if world[prevX][y] == ALIVE	{
 		counter++
 	}
-	if world[prevX][prevY] == 0xFF {
+	if world[prevX][prevY] == ALIVE {
 		counter++
 	}
-	if world[prevX][succY] == 0xFF {
+	if world[prevX][succY] == ALIVE {
 		counter++
 	}
-	if world[x][succY] == 0xFF {
+	if world[x][succY] == ALIVE {
 		counter++
 	}
-	if world[x][prevY] == 0xFF {
+	if world[x][prevY] == ALIVE {
 		counter++
 	}
-	if world[succX][y] == 0xFF {
+	if world[succX][y] == ALIVE {
 		counter++
 	}
-	if world[succX][succY] == 0xFF {
+	if world[succX][succY] == ALIVE {
 		counter++
 	}
-	if world[succX][prevY] == 0xFF {
+	if world[succX][prevY] == ALIVE {
 		counter++
 	}
 	return counter
+}
 
+func boundNumber(num int,worldLen int) int{
+	if num < 0 {
+		return num + worldLen
+	}else if num > worldLen - 1 {
+		return num - worldLen
+	}else{
+		return num
+	}
 }
